932ff143d7b50ade47f0e4a2c6cbd0b822da6e0b
fionicss <118982867+Fionicss@users.noreply.github.com>
2026-02-10 18:38:59 +0800

    Initial commit
    Adds setup code and folders for express app.

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('node:path');
+const customNotFoundError = require('./errors/customNotFoundError');
+const app = express();
+const PORT = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use((req, res, next) => {
+  throw new customNotFoundError('Page not found');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log('Now listening to port 3000!');
+});
---
+++ errors/customNotFoundError.js
@@ -0,0 +1,7 @@
+class customNotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+  }
+}
+module.exports = customNotFoundError;
