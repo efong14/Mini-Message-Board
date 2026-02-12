07bdf6c9fe128e0893e8b42ec78922c06889d505
fionicss <118982867+Fionicss@users.noreply.github.com>
2026-02-12 18:00:14 +0800

    Fixes route for new messages
    Removes new message router and merges it with index router. Creates
    router.post functionality for the form that adds to messages array and
    redirects to index page.

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const path = require('node:path');
 const customNotFoundError = require('./errors/customNotFoundError');
 const indexRouter = require('./routes/indexRouter');
-const newMessageRouter = require('./routes/newMessageRouter');
 const app = express();
 const PORT = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/', indexRouter);
-app.use('/new', newMessageRouter);
 
 app.use((req, res, next) => {
   throw new customNotFoundError('Page not found');
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -18,4 +18,16 @@ indexRouter.get('/', (req, res) => {
   res.render('displayMessage', { title: 'Mini Messageboard', messages: messages });
 });
 
+indexRouter.get('/new', (res, req) => {
+  res.render('form');
+});
+
+indexRouter.post('/new', (res, req) => {
+  const messageUser = req.body.user;
+  const messageText = req.body.message;
+  messages.push({ messages: messageText, user: messageUser, added: new Date() });
+
+  res.redirect('/');
+});
+
 module.exports = indexRouter;
